9ed03b32f6c3ad4cb9992406e68aced8a977ba78
Nate Festinger <nate.festinger@gmail.com>
2017-04-21 10:27:39 -0700

    Update documentation

--- js-unit-tester.js
+++ js-unit-tester.js
@@ -4,97 +4,26 @@
    
    a simple JS unit testing framework
    
-   adds assertEquals() and assertMethodEquals() to Function prototype
+   - adds assertEquals() and assertMethodEquals() methods onto Function prototype
+   
+   - comparison support for: 
+       - primitives (Number, String, Boolean, null, undefined)
+       - objects
+       - arrays 
+       (any other types encountered will result in unpredictable behavior)
+   
+   WARNING: Will mutate native Function prototype! */
 
-!!! TODOS
-    -----
-    - areEqual : exclude invalid types 
-*/
-
-function bracket(str) {
-  // return string enclosed in brackets
-  return '[' + str + ']';
-}
-
-Function.prototype.renderFailMsg = function(expected, actual, testName) {
-  var msg = 'FAILED ' + bracket(testName) + ': Expected ' + JSON.stringify(expected) + ', got ' + JSON.stringify(actual);
-  return msg;
-};
-
-Function.prototype.renderPassMsg = function(expected, actual, testName) {
-  var msg = 'passed ' + bracket(testName);
-  return msg; 
-}
-
-function areSameType(a, b) {
-  if (a == undefined || b == undefined) {
-    return a === b;
-  } else {
-    return a.constructor === b.constructor;
-  } 
-}
-
-function isPrimitive(obj) {
-  return typeof obj === 'number' || typeof obj === 'string' || typeof obj === 'boolean' ||
-    obj === undefined || obj === null; 
-}
-
-function countProperties(obj) {
-  var count = 0;
-  
-  for (var property in obj) {
-    ++count;
-  }
-  
-  return count;
-}
-
-function areEqual(a, b) {
-  // return true if a and b are 'equal' based on deep comparison
-  // a, b: primitives, arrays or objects
-  
-  if (!areSameType(a, b)) {
-    return false; 
-  }
-
-  // !!! TODO: check for invalid types
-  
-  if (isPrimitive(a)) {
-    return a === b;
-  } else if (a instanceof Array) {
-    var areSameLength = a.length === b.length; 
-    var containSameItems = a.every(function(item, index) {
-      return areEqual(item, b[index]);
-    });
-    
-    return areSameLength && containSameItems;
-    
-  } else { 
-    // else a, b are objects
-    var containSameNumberOfProps = countProperties(a) === countProperties(b);
-    
-    var propsContainSameValues = true;
-    
-    for (var prop in a) {
-      if (!areEqual(a[prop], b[prop])) {
-        propsContainSameValues = false;
-      }  
-    }
-    
-    return containSameNumberOfProps && propsContainSameValues;
-  }
-    
-}
 
 Function.prototype.assertEquals = function() {  
   
 /* run test and log result
-   for functions only; test methods with assertMethodEquals
+   for function testing only; test methods with assertMethodEquals()
 
- expects the following arguments, in order:
-    - arguments to pass to test function
+  expects the following arguments, in order:
+    - parameters to pass to test function
     - expected result
-    - test name (string)                     */
+    - test name (string) */
 
   var testName = arguments[arguments.length - 1];
   var expected = arguments[arguments.length - 2];
@@ -117,13 +46,13 @@ Function.prototype.assertEquals = function() {
 Function.prototype.assertMethodEquals = function() {  
   
 /* run test and log result
-   for functions only; test methods with assertMethodEquals
+   for method testing only; test functions with assertEquals()
 
- expects the following arguments, in order:
-    - binding object for method to act on
-    - arguments to pass to test function
+  expects the following arguments, in order:
+    - parent object for method to act on
+    - parameters to pass to test function
     - expected result
-    - test name (string)                     */
+    - test name (string) */
 
   var testName = arguments[arguments.length - 1];
   var expected = arguments[arguments.length - 2];
@@ -145,3 +74,87 @@ Function.prototype.assertMethodEquals = function() {
   } 
 }
 
+
+function areEqual(a, b) {
+  /* return true if a and b are 'equal', i.e. strictly equal (===) and contain identical values.
+     performs deep comparison on objects and arrays 
+     
+     ASSUMES: a, b are primitives (Number, String, Boolean, null, undefined), objects or arrays */
+  
+  if (!areSameType(a, b)) {
+    return false; 
+  }
+  
+  if (isPrimitive(a)) {
+    
+    return a === b;
+    
+  } else if (a instanceof Array) {
+    var areSameLength = a.length === b.length; 
+    var containSameItems = a.every(function(item, index) {
+      return areEqual(item, b[index]);
+    });
+    
+    return areSameLength && containSameItems;
+  
+  } else { 
+    // else a, b are objects
+    var containSameNumberOfProps = countProperties(a) === countProperties(b);
+    
+    var propsContainSameValues = true;
+    for (var prop in a) {
+      if (!areEqual(a[prop], b[prop])) {
+        propsContainSameValues = false;
+      }  
+    }
+    
+    return containSameNumberOfProps && propsContainSameValues;
+  }   
+}
+
+
+function areSameType(a, b) {
+  if (a === undefined || a === null) {
+    return a === b;
+  } else {
+    return a.constructor === b.constructor;
+  } 
+}
+
+
+function countProperties(obj) {
+  var count = 0;
+  
+  for (var property in obj) {
+    ++count;
+  }
+  
+  return count;
+}
+
+
+function isPrimitive(obj) {
+  /* return true if object is a primitive (Number, String, Boolean, undefined or null) */
+  
+  var PRIMITIVE_TYPES = ['number', 'string', 'boolean', 'undefined'];
+  
+  return obj === null || PRIMITIVE_TYPES.includes(typeof obj); 
+}
+
+
+Function.prototype.renderPassMsg = function(expected, actual, testName) {
+  var msg = 'passed ' + bracket(testName);
+  return msg; 
+}
+
+
+Function.prototype.renderFailMsg = function(expected, actual, testName) {
+  var msg = 'FAILED ' + bracket(testName) + ': Expected ' + JSON.stringify(expected) + ', got ' + JSON.stringify(actual);
+  return msg;
+};
+
+
+function bracket(str) {
+  // return string enclosed in brackets
+  return '[' + str + ']';
+}
